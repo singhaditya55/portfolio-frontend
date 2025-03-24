387a96c4aaf647456be4d39255025f02d140d316
singhaditya55 <singhaditya5298@gmail.com>
2025-03-24 19:29:52 +0530

    npm changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app"; // ✅ Ensure the correct casing (app.js)
+import App from "./App"; // ✅ Ensure the correct casing (app.js)
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
