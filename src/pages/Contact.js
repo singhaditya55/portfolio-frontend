8a144824aa23a582065bc5a5c5938cb53f77d3d3
singhaditya55 <singhaditya5298@gmail.com>
2025-03-28 22:14:14 +0530

    face rekognition push

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -38,7 +38,7 @@ export default function Contact() {
 };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white flex flex-col items-center justify-center p-6">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white flex flex-col items-center justify-center p-6 pt-20">
       <Navbar />
       
       <motion.h1
